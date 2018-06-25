7042165b38172096feb5a012b62ad475d2ef91d9
plesanc <plesanc@gmail.com>
2018-06-25 00:05:05 -0400

    New Features:  - Added basic parsing ability for both movies and shows  - VLC now launches returned media paths
    Features to Add:
     - Add control to VLC
      - Pause/Play
     - Add autoplay
     - Add and test casting to chromecast using mkchromecast
    
    Known Bugs:
     - Need to standardize file system naming conventions
      - remove all spaces
      - format season folders
      - format episode file names

--- pyvid.py
+++ pyvid.py
@@ -1,7 +1,7 @@
 import argparse
 import os
-
-# look further: https://pymotw.com/2/argparse/
+import time
+import vlc
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-v', action='store_true', default=False, dest='verbose')
@@ -14,35 +14,48 @@ parser.add_argument('-e', action='store', default=1, dest='episode')
 args = parser.parse_args()
 
 ## Find file in directory
-def find_file(path, name):
+def find_movie(path, name):
 	for root, dirs, files in os.walk(path):
 		for x in range(0, len(files)):
 			if name in files[x]:
 				return os.path.join(root, files[x])
 	return -1
 
-def find_dir(path, name, season):
+def find_show(path, name, season, episode):
+	#Look for show
 	for root, dirs, files in os.walk(path):
-		print(dirs)
+		if name in dirs:
+			print('Found show:', name)
+			path = os.path.join(path, name)
+			break
+		else:
+			print('Could not find show:', name)
+			return -1
 
-## Find media location
-def find_media(path, name, season, episode):
-	## Look for matching media
-	# for movie, have season = 0 & episode = 0
-	if (season == 0 & episode == 0):
-		# look for movie in root dir
-		return find_file(path, name)
-	else:
-		# look for a show
-		directory = find_dir(path, name, season)
-		return directory
-		# if (directory == -1)
+	#Look for season
+	season = 'Season_' + str(season)
+	for root, dirs, files in os.walk(path):
+		if season in dirs:
+			print('Found:', season)
+			path = os.path.join(path, season)
+			break
+		else:
+			print('Could not find season:', season)
+			return -1
 
+	#Look for episode
+	episode = 'E0' + str(episode)
+	for root, dirs, files in os.walk(path):
+		for x in range(0, len(files)):
+			if episode in files[x]:
+				return os.path.join(path, files[x])
+		print('Could not find Episode', episode)
+		return -1
 
-
+	return -1
 
 ## Main loop
-def main():
+def main_loop():
 
 	if (args.verbose):
 		print('cast', args.cast)
@@ -52,24 +65,49 @@ def main():
 		print('season', args.season)
 		print('episode', args.episode)
 
+	media_location = -1
+
 	if (args.movie_name != "disabled"):
 		# Look for movie
-		print("Looking for:", args.movie_name)
-		media_location = find_media('/home/chad/Videos/', args.movie_name, 0, 0)
+		print("Looking for Movie:", args.movie_name)
+		media_location = find_movie('/home/chad/Videos/', args.movie_name)
 
 	elif (args.show_name != "disabled"):
 		# Look for tv show
-		print("Looking for:", args.show_name, "Season", args.season, "Episode", args.episode)
-		media_location = find_media('/home/chad/Videos/', args.movie_name, args.season, args.episode)
+		print("Looking for TV Show:", args.show_name, "Season", args.season, "Episode", args.episode)
+		media_location = find_show('/home/chad/Videos/TV/', args.show_name, args.season, args.episode)
 	
+	# Check if succesfully found media
 	if (media_location == -1):
 		print("No File Found.")
 		return
 	print("Found media at: ", media_location)
 
-
-
 	if (args.cast):
 		print('Casting to Chromecast')
+		### do casting here....
+	else:
+		print('Launching VLC...')
+		
 
-main()
+		# Create instane of VLC and create reference to movie.
+		vlcInstance = vlc.Instance()
+		media = vlcInstance.media_new(media_location)
+		# Create new instance of vlc player
+		player = vlcInstance.media_player_new()
+		# Load movie into vlc player instance
+		player.set_media(media)
+
+		player.play()
+
+		#there are ways to pause etc through the vlc API, look into this..
+
+		while 1:
+			time.sleep(1)
+
+if __name__ == '__main__':
+    try:
+        main_loop()
+    except KeyboardInterrupt:
+        print >> sys.stderr, '\nExiting by user request.\n'
+        sys.exit(0)
