22d2402d84bb4c43d280574c401de588716c78cc
plesanc <plesanc@gmail.com>
2018-07-02 21:35:19 -0400

    New Features:  - Partial TV show names can now be parsed and found  - Tested some restructuring of file system  - Can handle leading zeros in season / episode numbers
    Features to Add:
     - Add control to VLC
     - Pause/Play
     - Add autoplay
     - Add and test casting to chromecast using mkchromecast
    
    Known Bugs:

--- pyvid.py
+++ pyvid.py
@@ -1,7 +1,19 @@
 import argparse
+import sys
 import os
 import time
 import vlc
+
+## USAGE
+#
+# Simple Movie:
+# python3 pyvid.py -m Watchmen
+#
+# TV Series from the start:
+# python3 pyvid.py -a -t Workaholics
+# 
+# Specific Episode:
+# python3 pyvid.py -a -t Workaholics -s 3 -e 12
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-v', action='store_true', default=False, dest='verbose')
@@ -17,23 +29,43 @@ args = parser.parse_args()
 def find_movie(path, name):
 	for root, dirs, files in os.walk(path):
 		for x in range(0, len(files)):
+			# Look for partial name matches
 			if name in files[x]:
 				return os.path.join(root, files[x])
 	return -1
 
 def find_show(path, name, season, episode):
-	#Look for show
+	# Look for show
+	show_found = False
 	for root, dirs, files in os.walk(path):
-		if name in dirs:
-			print('Found show:', name)
-			path = os.path.join(path, name)
-			break
-		else:
-			print('Could not find show:', name)
-			return -1
+		for x in range(0, len(dirs)):
+			# Look for partial name matches
+			if name in dirs[x]:
+				print('Found show:', dirs[x])
+				path = os.path.join(path, dirs[x])
+				show_found = True
+				break
 
-	#Look for season
+	if (show_found == False):
+		print('Could not find show:', name)
+		return -1
+
+	# Error check season number
+	season = str(season)
+	season_array = list(season)
+	if (len(season_array) > 2 or season == 0):
+		print('Invalid Season Number')
+		return -1
+
+	# Remove any leading zeros
+	if (season_array[0] == str(0)):
+		print('Removing Leading Zero')
+		season = season_array[1]
+
+	# Create parsable season string
 	season = 'Season_' + str(season)
+
+	# Look for season
 	for root, dirs, files in os.walk(path):
 		if season in dirs:
 			print('Found:', season)
@@ -43,8 +75,26 @@ def find_show(path, name, season, episode):
 			print('Could not find season:', season)
 			return -1
 
-	#Look for episode
-	episode = 'E0' + str(episode)
+	# Error check episode number
+	episode = str(episode)
+	episode_array = list(episode)
+	if (len(episode_array) > 2 or season == 0):
+		print('Invalid Season Number')
+		return -1
+
+	# Remove any leading zeros
+	if (episode_array[0] == str(0)):
+		print('Removing Leading Zero')
+		episode = episode_array[1]
+
+	# Create parsable episode string
+	print(len(episode))
+	if (len(episode) == 1):
+		episode = 'E0' + str(episode)
+	else:
+		episode = 'E' + str(episode)
+
+	# Look for episode
 	for root, dirs, files in os.walk(path):
 		for x in range(0, len(files)):
 			if episode in files[x]:
@@ -67,25 +117,32 @@ def main_loop():
 
 	media_location = -1
 
+	# Look for movie
 	if (args.movie_name != "disabled"):
-		# Look for movie
-		print("Looking for Movie:", args.movie_name)
+		print('Looking for Movie:', args.movie_name)
 		media_location = find_movie('/home/chad/Videos/', args.movie_name)
 
+	# Look for tv show
 	elif (args.show_name != "disabled"):
-		# Look for tv show
-		print("Looking for TV Show:", args.show_name, "Season", args.season, "Episode", args.episode)
+		print('Looking for TV Show:', args.show_name, 'Season', args.season, 'Episode', args.episode)
 		media_location = find_show('/home/chad/Videos/TV/', args.show_name, args.season, args.episode)
 	
 	# Check if succesfully found media
 	if (media_location == -1):
-		print("No File Found.")
+		print('No File Found.')
 		return
-	print("Found media at: ", media_location)
+	print('Found media at: ', media_location)
 
+	# Cast media to chromcast
 	if (args.cast):
 		print('Casting to Chromecast')
 		### do casting here....
+
+		# while media is playing it would be helpful to be able to pass commands through to mkchromecast
+		while 1:
+			time.sleep(1)
+
+	# Watch media locally using VLC
 	else:
 		print('Launching VLC...')
 		
@@ -109,5 +166,5 @@ if __name__ == '__main__':
     try:
         main_loop()
     except KeyboardInterrupt:
-        print >> sys.stderr, '\nExiting by user request.\n'
+        print('\nExitting by keyboard interrupt.\n')
         sys.exit(0)
